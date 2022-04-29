d39af3368e0b31c1b9e07a64924e8769ed8365b4
Antony Beardsmore <sider@me.com>
2022-04-29 21:28:45 +0100

    timer added

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	// Provides helper flags when using CLI eg "./quiz --help"
 	csvFilename := flag.String("csv", "problems.csv", "CSV file in format of 'question,answer'")
 	flag.Parse()
+
+	// Create time limit flag
+	timeLimit := flag.Int("limit", 30, "The time limit for the quiz in seconds")
 
 	// Open the CSV file
 	file, err := os.Open(*csvFilename) // (*)- use value not pointer
@@ -27,16 +31,30 @@ func main() {
 		exit("Failed to parse the CSV file provided")
 	}
 	problems := parseLines(lines)
-	fmt.Println(problems)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	correct := 0
+	// <-timer.C // program stops until message from channel
 
 	// Print problems
-	correct := 0
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer) // & - pointer NOT value
-		if answer == p.a {
-			correct++
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() { // Annonymous function
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C: // execute when msg from timer is rec'd
+			fmt.Printf("Your score is %d out of %d. \n", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == p.a {
+				correct++
+			}
+
 		}
 	}
 	fmt.Printf("Your score is %d out of %d. \n", correct, len(problems))
